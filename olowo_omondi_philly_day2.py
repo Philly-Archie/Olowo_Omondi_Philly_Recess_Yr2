1bd980e4ec900688d6f50e28a8e41afc4d770a0c
Philly Olowo <olowophilly11@gmail.com>
2024-06-07 11:02:59 +0300

    first commit

---
+++ olowo_omondi_philly_day2.py
@@ -0,0 +1,26 @@
+# Exercise 
+
+# age = 20
+
+# if age <13:
+#     print("Minor's movie Discount is Shs 1000")
+
+# elif age > 13 and age < 18:
+#     print("Teenager movie discount is Shs 500")
+
+# elif age > 18 and age <65:
+#     print("Adults pay price of Shs 2000")
+
+# else:
+#     print("Senior adults pay of Shs 5000")
+
+
+# Loops
+
+favorite_colors = []
+
+x= 5
+
+while x<0:
+    color = input("Enter favorite color: ")
+    print (favorite_colors[color])
