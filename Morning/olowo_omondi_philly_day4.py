96cc87c393eaef746e2e73ea689f7c1020a6ab00
Philly Olowo <olowophilly11@gmail.com>
2024-06-12 09:32:18 +0300

    Added day 4 classwork file

---
+++ Morning/olowo_omondi_philly_day4.py
@@ -0,0 +1,26 @@
+# Dictionaries
+# Creating amd using dictionaries
+# Dictionary methods and operations
+
+
+"""
+Dictionaries in pythons are collections of keys and values.
+-Unordered
+-Mutable
+-Indexed by keys
+
+"""
+
+# Create a dictionary for a student pursuing software engineering,
+# Key must be name, age technology interest, course and year of study
+# Put your own details
+
+context = {
+    'name' : 'Olowo Omondi Philly',
+    'age' : 24,
+    'technology interest' : 'cyber_security',
+    'course' : 'Software engineering',
+    'Year of study' : 2,
+}
+
+print(context['age'])
