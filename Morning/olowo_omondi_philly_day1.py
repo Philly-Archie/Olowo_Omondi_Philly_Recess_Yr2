56009d8c0e8fec4bde9a589837cd7982ee980d9c
Philly Olowo <olowophilly11@gmail.com>
2024-07-11 14:21:59 +0300

    Most update of classwork files

---
+++ Morning/olowo_omondi_philly_day1.py
@@ -0,0 +1,15 @@
+# OLOWO OMONDI PHILLY
+# REG NO 22/U/20919
+# STUDENT NO 2200720919
+
+# SYNTAX AND COMMENTS ASSIGNMENT
+# Day 1
+
+print("OLOWO OMONDI PHILLY")
+
+First_name = input("what is your first name?")
+Favourite_color = input("what is your favourite color?")
+
+print(
+    First_name + " " + "prefers " + " " + Favourite_color + " " + "to any other colour"
+)
